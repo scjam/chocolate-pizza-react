b3f73c54d9141b518bb3c40ef70703cae892f024
Richard Hillman <Richard.CC.Hillman@gmail.com>
2020-10-26 16:09:25 -0700

    added more website stuff links main body pics connected and moved to public

--- src/SydneysAmazingHeader.js
+++ src/SydneysAmazingHeader.js
@@ -1,10 +1,15 @@
 // eslint-disable-next-line
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import SmallLogo from './assets/small-logo.png';
 
 export default class SydneysAmazingHeader extends React.Component {
     render() {
         return (
             <div>
+                <span> 
+                    <img src={SmallLogo} alt="small logo with a fork" />
+                </span>
+                
                 <span className='delicious inblock'>
                     <h1>DELICIOUS</h1>
                     <h2>THE BEST CHOCOLATE PIZZA EVER</h2>
