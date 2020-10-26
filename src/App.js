9f97e429ae8875b66e9a440507e2ba3241c986ef
Sydney James <sydneyjames7@gmail.com>
2020-10-26 15:32:07 -0700

    footer working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import SydneysAmazingHeader from './SydneysAmazingHeader.js'
+import SydneysAmazingHeader from './SydneysAmazingHeader.js';
+import RichardsIncredibleFooter from './RichardsIncredibleFooter.js';
 
 export default class App extends React.Component {
   render() {
     return (
     <div>
       <SydneysAmazingHeader />
+      <RichardsIncredibleFooter />
     </div>
     );
   }
